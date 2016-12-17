ef2c1aa947517a4ee2913cf4004fe5f07b1b004f
Konrad R.K. Ludwig <konrad.rk.ludwig@gmail.com>
2016-12-17 03:35:24 -0800

    Add groups field to and tests for UpdateUser and CreateUser schema

--- stackcite_api/api/users/schemas.py
+++ stackcite_api/api/users/schemas.py
@@ -1,13 +1,33 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validates, ValidationError
+
+from stackcite.data import User
 
 from stackcite_api.api.schemas import fields as api_fields
+
+
+def _validate_default_groups(value):
+    for default_group in User.DEFAULT_GROUPS:
+        if default_group not in value:
+            msg = 'Default group missing: {} not in {}'.format(
+                default_group, value)
+            raise ValidationError(msg)
 
 
 class UpdateUser(Schema):
     email = fields.Email()
     password = api_fields.PasswordField()
+    groups = fields.List(api_fields.GroupField())
+
+    @validates('groups')
+    def validate_groups(self, value):
+        _validate_default_groups(value)
 
 
 class CreateUser(Schema):
     email = fields.Email(required=True)
     password = api_fields.PasswordField(required=True)
+    groups = fields.List(api_fields.GroupField())
+
+    @validates('groups')
+    def validate_groups(self, value):
+        _validate_default_groups(value)
---
+++ stackcite_api/api/users/tests/test_schemas.py
@@ -0,0 +1,94 @@
+import unittest
+
+from stackcite_api import testing
+
+
+class UpdateUserTests(unittest.TestCase):
+
+    layer = testing.layers.UnitTestLayer
+
+    def setUp(self):
+        from ..schemas import UpdateUser
+        self.schema = UpdateUser()
+
+    def test_invalid_group_fails_validation(self):
+        """UpdateUser schema returns an error for an invalid group
+        """
+        from stackcite_api import auth
+        data = {'groups': ['invalid', auth.STAFF]}
+        result = self.schema.load(data).errors.keys()
+        self.assertIn('groups', result)
+
+    def test_missing_users_group_fails_validation(self):
+        """UpdateUser schema returns an error if default group is missing
+        """
+        from stackcite_api import auth
+        data = {'groups': [auth.STAFF, auth.ADMIN]}
+        result = self.schema.load(data).errors.keys()
+        self.assertIn('groups', result)
+
+    def test_valid_groups_pass_validation(self):
+        """UpdateUser schema does not return an error if all groups are valid
+        """
+        from stackcite_api import auth
+        data = {'groups': [auth.USERS, auth.STAFF, auth.ADMIN]}
+        result = self.schema.load(data).errors.keys()
+        self.assertNotIn('groups', result)
+
+    def test_groups_optional(self):
+        """UpdateUser schema does not return an error if no groups are defined
+        """
+        result = self.schema.load({}).errors.keys()
+        self.assertNotIn('groups', result)
+
+
+class CreateUserTests(unittest.TestCase):
+
+    layer = testing.layers.UnitTestLayer
+
+    def setUp(self):
+        from ..schemas import CreateUser
+        self.schema = CreateUser()
+
+    def test_email_required(self):
+        """CreateUser schema returns an error if no email is provided
+        """
+        result = self.schema.load({}).errors.keys()
+        self.assertIn('email', result)
+
+    def test_password_required(self):
+        """CreateUser schema returns an error if no email is provided
+        """
+        result = self.schema.load({}).errors.keys()
+        self.assertIn('password', result)
+
+    def test_invalid_group_fails_validation(self):
+        """CreateUser schema returns an error for an invalid group
+        """
+        from stackcite_api import auth
+        data = {'groups': ['invalid', auth.STAFF]}
+        result = self.schema.load(data).errors.keys()
+        self.assertIn('groups', result)
+
+    def test_missing_users_group_fails_validation(self):
+        """CreateUser schema returns an error if default group is missing
+        """
+        from stackcite_api import auth
+        data = {'groups': [auth.STAFF, auth.ADMIN]}
+        result = self.schema.load(data).errors.keys()
+        self.assertIn('groups', result)
+
+    def test_valid_groups_pass_validation(self):
+        """CreateUser schema does not return an error if all groups are valid
+        """
+        from stackcite_api import auth
+        data = {'groups': [auth.USERS, auth.STAFF, auth.ADMIN]}
+        result = self.schema.load(data).errors.keys()
+        self.assertNotIn('groups', result)
+
+    def test_groups_optional(self):
+        """CreateUser schema does not return an error if no groups are defined
+        """
+        result = self.schema.load({}).errors.keys()
+        self.assertNotIn('groups', result)
+
