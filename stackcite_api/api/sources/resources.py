04298c06703abf177749cf68d937a2fb9b158e86
Konrad R.K. Ludwig <konrad.rk.ludwig@gmail.com>
2016-12-17 05:28:40 -0800

    Update sources resources to use new ValidatedResource interface

--- stackcite_api/api/sources/resources.py
+++ stackcite_api/api/sources/resources.py
@@ -7,38 +7,50 @@ from . import schema
 
 class SourceDocument(api.resources.APIDocument):
 
-    _update_schema = schema.UpdateSource
+    _schema = {
+        'PUT': schema.UpdateSource
+    }
 
 
 class SourceCollection(api.resources.APICollection):
 
+    _schema = {
+        'POST': schema.CreateSource
+    }
+
     _collection = db.Source
     _document_resource = SourceDocument
-
-    _create_schema = schema.CreateSource
 
 
 class TextSourceDocument(api.resources.APIDocument):
 
-    _update_schema = schema.UpdateTextSource
+    _schema = {
+        'PUT': schema.UpdateTextSource
+    }
 
 
 class TextSourceCollection(api.resources.APICollection):
 
+    _schema = {
+        'POST': schema.CreateTextSource
+    }
+
     _collection = db.TextSource
     _document_resource = TextSourceDocument
-
-    _create_schema = schema.CreateTextSource
 
 
 class BookSourceDocument(api.resources.APIDocument):
 
-    _update_schema = schema.UpdateBookSource
+    _schema = {
+        'PUT': schema.UpdateBookSource
+    }
 
 
 class BookSourceCollection(api.resources.APICollection):
 
+    _schema = {
+        'POST': schema.CreateBookSource
+    }
+
     _collection = db.BookSource
     _document_resource = BookSourceDocument
-
-    _create_schema = schema.CreateBookSource
