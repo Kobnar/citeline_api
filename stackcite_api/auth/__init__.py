6d56ff40ac84c03f78646e89b56fd789006e99fa
Konrad R.K. Ludwig <konrad.rk.ludwig@gmail.com>
2016-12-17 02:52:35 -0800

    Use stackcite.auth to define user groups

--- stackcite_api/auth/__init__.py
+++ stackcite_api/auth/__init__.py
@@ -1,6 +1,7 @@
+from stackcite.data.json import auth as _auth
+
 from .utils import get_token, get_user
 from .policies import TokenAuthenticationPolicy
 
-USERS = 'users'
-STAFF = 'staff'
-ADMIN = 'admin'
+GROUPS = _auth.GROUPS
+USERS, STAFF, ADMIN = GROUPS
