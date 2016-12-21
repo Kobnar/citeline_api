7ab00d4c3d3310832a22cbd4cffe0594f46e0db7
Kobnar <konrad.rk.ludwig@gmail.com>
2016-12-21 00:15:02 -0800

    Add 'key' field to Authenticate schema

--- stackcite_api/api/auth/schema.py
+++ stackcite_api/api/auth/schema.py
@@ -1,12 +1,26 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validates_schema, ValidationError
 
 from stackcite_api.schema import fields as api_fields
 
 
 class Authenticate(Schema):
-    email = fields.Email(required=True)
-    password = api_fields.PasswordField(required=True)
+    email = fields.Email()
+    password = api_fields.PasswordField()
+    key = api_fields.TokenKeyField()
+
+    @validates_schema
+    def validate_dependant_fields(self, data):
+        err_msg = fields.Field.default_error_messages['required']
+        missing_fields = []
+        if not data.get('key'):
+            if not data.get('email'):
+                missing_fields.append('email')
+            if not data.get('password'):
+                missing_fields.append('password')
+        if missing_fields:
+            raise ValidationError(err_msg, field_names=missing_fields)
 
 
 class Token(Schema):
+    # TODO: This should be "key"
     token = api_fields.TokenKeyField(required=True)
--- stackcite_api/api/auth/tests/test_schema.py
+++ stackcite_api/api/auth/tests/test_schema.py
@@ -12,12 +12,25 @@ class AuthenticateTests(unittest.TestCase):
         self.schema = Authenticate()
 
     def test_email_field_required(self):
+        """Authenticate.email is a required field
+        """
         result = self.schema.load({}).errors.keys()
         self.assertIn('email', result)
 
     def test_password_field_required(self):
+        """Authenticate.password is a required field
+        """
         result = self.schema.load({}).errors.keys()
         self.assertIn('password', result)
+
+    def test_email_password_optional_if_key_set(self):
+        """Authenticate.key makes 'email' and 'password' fields optional
+        """
+        data = {
+            'key': '3ba7b73edb07bbc8f5ee7a642a27bba555b33bbf8fffd7669f3d2af4'}
+        result = self.schema.load(data).errors.keys()
+        self.assertNotIn('email', result)
+        self.assertNotIn('password', result)
 
 
 class TokenTests(unittest.TestCase):
@@ -29,5 +42,7 @@ class TokenTests(unittest.TestCase):
         self.schema = Token()
 
     def test_token_field_required(self):
+        """Authenticate.token is a required field
+        """
         result = self.schema.load({}).errors.keys()
         self.assertIn('token', result)
