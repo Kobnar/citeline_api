61cf4b43946f54506e8d7086f02163855fea915a
Kobnar <konrad.rk.ludwig@gmail.com>
2016-12-21 07:23:34 -0800

    Commit half-baked ConfirmationResource because there is a better way

--- stackcite_api/api/users/resources.py
+++ stackcite_api/api/users/resources.py
@@ -1,3 +1,4 @@
+import logging
 from pyramid import security as sec
 
 from stackcite import data as db
@@ -5,6 +6,38 @@ from stackcite import data as db
 from stackcite_api import resources, auth
 
 from . import schema
+
+
+_LOG = logging.getLogger(__name__)
+
+
+class ConfirmationResource(
+        resources.APIIndexResource, resources.ValidatedResource):
+
+    _SCHEMA = {
+        'create': schema.CreateConfirmationToken,
+        'update': schema.ConfirmConfirmationToken
+    }
+
+    def create(self, data):
+        """
+        Creates a new confirmation token and dispatches an email request to
+        the end-user.
+        """
+        data, errors = self.validate('create', data)
+        user_id = data['user']
+        user = db.User.objects.get(id=user_id)
+        token = db.ConfirmToken.new(user, save=True)
+        # Dispatch new email
+
+        # If user is already confirmed, return 409 CONFLICT
+
+    def update(self):
+        """
+        Confirms a user's registration.
+        """
+        # Accept confirmation key
+        # Confirm user account
 
 
 class UserDocument(resources.APIDocumentResource):
@@ -15,6 +48,10 @@ class UserDocument(resources.APIDocumentResource):
             (sec.Allow, auth.ADMIN, ('retrieve', 'update', 'delete')),
             sec.DENY_ALL
         ]
+
+    _OFFSPRING = {
+        'confirmation': ConfirmationResource
+    }
 
     _SCHEMA = {
         'PUT': schema.UpdateUser
@@ -35,3 +72,9 @@ class UserCollection(resources.APICollectionResource):
     _SCHEMA = {
         'POST': schema.CreateUser
     }
+
+    def create(self, data):
+        user = super().create(data)
+        conf_token = db.ConfirmToken.new(user, save=True)
+        _LOG.info('New confirmation token: {}'.format(conf_token.key))
+        return user
