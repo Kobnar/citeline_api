86168681148b8f18632bcb6ad5ca10484e7bc57d
Kobnar <konrad.rk.ludwig@gmail.com>
2016-12-20 21:54:22 -0800

    Add utility methods in 'testing' to quickly create test data in database

--- stackcite_api/testing/__init__.py
+++ stackcite_api/testing/__init__.py
@@ -1,6 +1,9 @@
-from stackcite.testing import data
+from stackcite.testing import data as _data
 
 from . import endpoint
 from . import layers
 from . import mock
+from . import utils
 from . import views
+
+data = _data
