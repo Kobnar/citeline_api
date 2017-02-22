704d4a319c3fb6fc4dbe6fdd72acd33bb8ce0dcf
Kobnar <konrad.rk.ludwig@gmail.com>
2017-02-22 07:13:42 +0000

    Add description to organization schemas

--- stackcite/api/index/organizations/schema.py
+++ stackcite/api/index/organizations/schema.py
@@ -6,11 +6,13 @@ from stackcite.api.schema import forms as api_forms
 class UpdateOrganization(Schema):
     name = fields.String()
     established = fields.Integer(allow_none=True)
+    description = fields.String()
 
 
 class CreateOrganization(Schema):
     name = fields.String(required=True)
     established = fields.Integer(allow_none=True)
+    description = fields.String()
 
 
 class RetrieveOrganizations(api_forms.RetrieveCollection):
@@ -21,9 +23,11 @@ class UpdatePublisher(Schema):
     name = fields.String()
     established = fields.Integer()
     region = fields.String(validate=lambda r: len(r) == 2)
+    description = fields.String()
 
 
 class CreatePublisher(Schema):
     name = fields.String(required=True)
     established = fields.Integer()
     region = fields.String(validate=lambda r: len(r) == 2)
+    description = fields.String()
